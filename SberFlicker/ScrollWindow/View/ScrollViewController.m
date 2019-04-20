30951e71e6e9e2d24f92b31fbef549a82f5c50b5
StJumbo <sergejgryzin@MacBook-Pro-Sergej.local>
2019-04-20 21:55:37 +0300

    CIFilter usage

--- SberFlicker/ScrollWindow/View/ScrollViewController.m
+++ SberFlicker/ScrollWindow/View/ScrollViewController.m
@@ -14,6 +14,9 @@
 
 @property (nonatomic, strong)PhotoModel *picture;
 @property (nonatomic, strong)ScrollPresenter *presenter;
+@property (nonatomic, strong)UIImage *image;
+@property (nonatomic, strong)UIImageView *imageView;
+@property (nonatomic, strong)CIFilter *filter;
 
 @end
 
@@ -29,18 +32,20 @@
     [self.view addSubview:activityIndicator];
     [activityIndicator setCenter:self.view.center];
     [activityIndicator startAnimating];
+    
     if (self.picture.pictureURL)
     {
         [self.presenter.netwotkDelegate getImageFromURL:self.picture.pictureURL completion:^(UIImage * _Nonnull picture) {
             
             dispatch_async(dispatch_get_main_queue(), ^{
-                UIImageView *imageView = [[UIImageView alloc] initWithImage:picture];
+                self.imageView = [[UIImageView alloc] initWithImage:picture];
                 UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-                [scrollView addSubview:imageView];
+                [scrollView addSubview:self.imageView];
                 [scrollView setScrollEnabled:YES];
-                [scrollView setContentSize:imageView.bounds.size];
+                [scrollView setContentSize:self.imageView.bounds.size];
                 
                 [self.view addSubview:scrollView];
+                self.image = picture;
                 [activityIndicator stopAnimating];
             });
         }];
@@ -49,7 +54,14 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    
     [self removeSearchBar];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self addFilterButton];
 }
 
 - (void)removeSearchBar
@@ -68,6 +80,56 @@
 - (void)setCurrentPicture:(PhotoModel *)picture
 {
     self.picture = picture;
+}
+
+- (void)addFilterButton
+{
+    UIBarButtonItem *filterButton = [[UIBarButtonItem alloc] initWithTitle:@"Random filter" style:UIBarButtonItemStylePlain target:self action:@selector(setFilter)];
+    [filterButton setWidth:100.0f];
+    [self.navigationController.navigationBar.topItem setRightBarButtonItem:filterButton];
+}
+
+- (void)setFilter
+{
+    self.filter = nil;
+    if (self.image)
+    {
+        CIImage *image = [[CIImage alloc] initWithImage:self.image];
+        NSInteger index = arc4random() % 5;
+        self.filter = [self filterWithType:index];
+        [self.filter setValue:image forKey:kCIInputImageKey];
+    }
+    CIImage *result = [self.filter valueForKey: @"outputImage"];
+    CGImageRef cgImageRef = [[CIContext contextWithOptions:nil] createCGImage:result fromRect:[result extent]];
+    UIImage *targetImage = [UIImage imageWithCGImage:cgImageRef];
+    self.imageView.image = targetImage;
+    self.image = targetImage;
+    
+}
+
+- (CIFilter *)filterWithType:(NSInteger)type
+{
+    switch (type) {
+        case 0:
+            return [CIFilter filterWithName:@"CIPhotoEffectTonal"];
+            break;
+        case 1:
+            return [CIFilter filterWithName:@"CIPhotoEffectTransfer"];
+            break;
+        case 2:
+            return [CIFilter filterWithName:@"CIPhotoEffectProcess"];
+            break;
+        case 3:
+            return [CIFilter filterWithName:@"CIPhotoEffectNoir"];
+            break;
+        case 4:
+            return [CIFilter filterWithName:@"CIColorMonochrome"];
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
 }
 
 @end
